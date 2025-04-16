8a6d64cd28ef46c5e4539fd750f33e28a09282d0
Victor-Suander <victors.empresa@gmail.com>
2025-04-15 22:52:49 -0300

    pmanager version 0.2

---
+++ pmanager/src/main/java/com/java360/pmanager/domain/applicationservice/ProjectService.java
@@ -0,0 +1,47 @@
+package com.java360.pmanager.domain.applicationservice;
+
+import com.java360.pmanager.domain.entity.Project;
+import com.java360.pmanager.domain.exception.ProjectNotFoundException;
+import com.java360.pmanager.domain.model.ProjectStatus;
+import com.java360.pmanager.domain.repository.ProjectRepository;
+import com.java360.pmanager.infrastructure.dto.SaveProjectDataDTO;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    @Transactional
+    public Project createProject(SaveProjectDataDTO saveProjectData) {
+        Project project = Project
+                .builder()
+                .name(saveProjectData.getName())
+                .description(saveProjectData.getDescription())
+                .initialDate(saveProjectData.getInitialDate())
+                .finalDate(saveProjectData.getFinalDate())
+                .status(ProjectStatus.PENDING)
+                .build();
+
+        projectRepository.save(project);
+
+        log.info("Project created: {}", project);
+        return project;
+
+    }
+
+    public Project loadProject(String projectId) {
+        return projectRepository
+                .findById(projectId)
+                .orElseThrow(() -> new ProjectNotFoundException(projectId));
+    }
+}
---
+++ pmanager/src/main/java/com/java360/pmanager/domain/exception/ProjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.java360.pmanager.domain.exception;
+
+import com.java360.pmanager.infrastructure.exception.RequestException;
+
+public class ProjectNotFoundException extends RequestException {
+
+    public ProjectNotFoundException(String projectId) {
+        super("ProjectNotFound", "Project not found: " + projectId);
+    }
+
+}
